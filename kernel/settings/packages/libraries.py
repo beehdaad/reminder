74e80f2be664d8d9cb100d8e3aafda5d4a3b961b
beehdaad <behdad.siavoshi1375@gmail.com>
2023-07-12 11:29:51 +0330

    feature(todo_list): Add todo_list app and changing its structure

--- kernel/settings/packages/libraries.py
+++ kernel/settings/packages/libraries.py
@@ -1,7 +1,8 @@
 from kernel.settings.base import DEFAULT_APPS
 
 BUSINESS_APPS = [
-    'painless.apps.PainlessConfig'
+    'painless.apps.PainlessConfig',
+    'todo_list.apps.TodoListConfig'
 ]
 
 INSTALLED_APPS =  DEFAULT_APPS + BUSINESS_APPS
