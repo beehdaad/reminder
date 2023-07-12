4810ac1296543f1ffb7bded778766abe500e3c6b
beehdaad <behdad.siavoshi1375@gmail.com>
2023-07-13 00:58:09 +0330

    refactor(todo_list/helper): Remove cancel choice

--- todo_list/helper/choices.py
+++ todo_list/helper/choices.py
@@ -5,4 +5,3 @@ from django.utils.translation import gettext_lazy as _
 class TodoStatus(models.TextChoices):
     IN_PROGRESS = ('in progress', _('In Progress'))
     COMPLETED = ('completed', _('Completed'))
-    CANCEL = ('cancel', _('Cancel'))
